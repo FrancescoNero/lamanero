1fe1940e2ca5b1fb4bf4307f1652a38e6e67208d
student <student@student.com>
2013-05-31 23:07:10 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lamanero::Application.config.secret_token = 'a48a1223c753ec9fc0d2312e1ef8b80a51e2d2d120b20c306a1185c540ae8cbe6f68a89189e946d3b6c9652420635393ad207d4f6377cafcdfe996f94adca979'
