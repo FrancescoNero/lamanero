21317f7e39f04783bddd814873486859460bb005
Francesco Nero <nerofrancesco@hotmail.com>
2013-06-12 14:45:59 +0200

    Added first relation Metrocubo->London

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :tech_info, :title, :type, :room
-  has_and_belongs_to_many :retailers, :inverse_of => :product
+  has_and_belongs_to_many :retailers
 end
--- app/models/retailer.rb
+++ app/models/retailer.rb
@@ -1,4 +1,4 @@
 class Retailer < ActiveRecord::Base
   attr_accessible :description, :latitude, :longitude, :title, :address, :city, :telephone, :email
-  has_and_belongs_to_many :products, :inverse_of => :retailer
+  has_and_belongs_to_many :products
 end
